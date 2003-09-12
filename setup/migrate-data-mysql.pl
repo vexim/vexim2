fd02c2f4bac5faa70d62f209a50506024d29b88e
avleen <avleen>
2003-09-12 21:06:49 +0000

    Script to migrate data from old MySQL database to the new one.

---
+++ setup/migrate-data-mysql.pl
@@ -0,0 +1,80 @@
+#!/usr/local/bin/perl
+
+use strict;
+use DBI();
+my $dbh = DBI->connect("DBI:mysql:database=vexim;host=localhost",
+                       "USER", "PASS",
+                       {'RaiseError' => 1});
+
+#print "Dropping already present tables...\n";
+#$dbh->do("DROP TABLE IF EXISTS domains");
+#$dbh->do("DROP TABLE IF EXISTS users");
+print "Creating new tables...\n";
+$dbh->do("CREATE TABLE IF NOT EXISTS domains (domain_id mediumint(8) unsigned NOT NULL auto_increment,
+	domain varchar(64) NOT NULL default '',
+	spamassassin bool NOT NULL default '0',
+	avscan bool NOT NULL default '0',
+	mailinglists bool NOT NULL default '0',
+	quotas int(10) unsigned NOT NULL default '0',
+	blocklists bool NOT NULL default '0',
+	uid smallint(5) unsigned NOT NULL default '65534',
+	gid smallint(5) unsigned NOT NULL default '65534',
+	complexpass bool NOT NULL default '0',
+	PRIMARY KEY  (domain_id),
+	UNIQUE KEY domain (domain),
+	KEY domain_id (domain_id),
+	KEY domains (domain))") or die "Could not create table domains";
+$dbh->do("CREATE TABLE IF NOT EXISTS users (user_id int(10) unsigned NOT NULL auto_increment,
+	domain_id mediumint(8) unsigned NOT NULL,
+	localpart varchar(192) NOT NULL default '',
+	clear varchar(255) default NULL,
+	crypt varchar(48) default NULL,
+	uid smallint(5) unsigned NOT NULL default '0',
+	gid smallint(5) unsigned NOT NULL default '0',
+	smtp varchar(255) NOT NULL default '',
+	pop varchar(255) NOT NULL default '',
+	realname varchar(255) NOT NULL default '',
+	type enum('local','alias','catch', 'piped') NOT NULL default 'local',
+	admin bool NOT NULL default '0',
+	avscan bool NOT NULL default '0',
+	blocklist bool NOT NULL default '0',
+	complexpass bool NOT NULL default '0',
+	quota int(10) unsigned NOT NULL default '0',
+	sa_tag smallint(5) unsigned NOT NULL default '5',
+	sa_refuse smallint(5) unsigned NOT NULL default '10',
+	on_vacation bool NOT NULL default '0',
+	vacation varchar(255) default NULL,
+	flags varchar(16) default NULL,
+	tagline varchar(255) default NULL,
+	PRIMARY KEY  (user_id),
+	UNIQUE KEY username (localpart,domain_id),
+	KEY local (localpart))") or die "Could not create table users";
+
+print "Tables created. Starting domain migration...\n";
+$dbh->do("INSERT INTO vexim.domains (domain) SELECT DISTINCT domain FROM exim.users ORDER BY domain");
+print "Domain migration complete.\n\n";
+
+print " Starting user migration...\n";
+my $sth = $dbh->prepare("SELECT DISTINCT domain_id,domain FROM vexim.domains");
+$sth->execute();
+while (my $ref = $sth->fetchrow_hashref()) {
+  my $domain_id = $ref->{'domain_id'};
+  my $domain = $ref->{'domain'};
+  print "\tMigrating users for Domain: $domain\n";
+  my $lkp = $dbh->prepare("INSERT INTO users (domain_id, localpart, clear, crypt, uid, gid, smtp, pop, realname, type, admin)
+		SELECT '$domain_id' AS domain_id,
+		local_part AS localpart,
+		cpassword AS clear,
+		password AS crypt,
+		t1.uid,
+		t1.gid,
+		smtphome AS smtp,
+		pophome AS pop,
+		realname,
+		type,
+		admin FROM exim.users t1 INNER JOIN vexim.domains t2 ON t1.domain=t2.domain where
+		t2.domain='$domain'") or die "Error migrating users for $domain";
+  $lkp->execute();
+}
+$sth->finish();
+
