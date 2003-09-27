06264ff77b16234768a110e01bb677996143088a
avleen <avleen>
2003-09-27 18:42:38 +0000

    added AllowUserLogin and re-orderd the variable list

--- vexim/config/variables.php
+++ vexim/config/variables.php
@@ -12,11 +12,11 @@
   if (DB::isError($db)) { die ($db->getMessage()); }
   $db->setFetchMode(DB_FETCHMODE_ASSOC);
  
-  /* URL to your Virtual Exim install */
-  $veximurl = "vexim2.silverwraith.com";
+  /* Allow users to log in? */
+  $AllowUserLogin = "yes";
 
   /* Set to either "des" or "md5" depending on your crypt() libraries */
-  $cryptscheme = "md5";
+  $crypto = "md5";
 
   /* Default password to use for things like Aliases. */
   $emptypass = "NOPASSWORD"; 
@@ -27,10 +27,13 @@
   /* path to Mailman */
   $mailmanroot = "http://www.EXAMPLE.com/mailman";
 
+  /* URL to your Virtual Exim install */
+  $veximurl = "vexim2.silverwraith.com";
+
   /* Welcome message, sent to new POP/IMAP accounts */
   $welcome_message = "Welcome, $_POST[realname] !\n\nYour new E-mail account is all ready for you.\n\n"
                      . "Here are some settings you might find useful:\n\n"
-		     . "Username: $_POST[username]@" . $_COOKIE[vexim][2] ."\n"
+		     . "Username: $_POST[localpart]@" . $_COOKIE[vexim][2] ."\n"
 		     . "POP3 server: mail." . $_COOKIE[vexim][2] . "\n"
 		     . "SMTP server: mail." . $_COOKIE[vexim][2] . "\n";
 
