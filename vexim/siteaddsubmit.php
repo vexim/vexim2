468a2af21104231f7a13e3bf7eceb5c39a5e921b
avleen <avleen>
2003-11-21 01:58:17 +0000

    fixed missing =

--- vexim/siteaddsubmit.php
+++ vexim/siteaddsubmit.php
@@ -7,7 +7,7 @@
   if (isset($_POST[spamassassin])) {$_POST[spamassassin] = 1;} else {$_POST[spamassassin] = 0;}
   if (isset($_POST[enabled])) {$_POST[enabled] = 1;} else {$_POST[enabled] = 0;}
   if (isset($_POST[pipe])) {$_POST[pipe] = 1;} else {$_POST[pipe] = 0;}
-  if ($_POST[type] = "relay") {$_POST[clear] = $_POST[vclear] = "BLANK";}
+  if ($_POST[type] == "relay") {$_POST[clear] = $_POST[vclear] = "BLANK";}
 
   $smtphomepath = $mailroot . $_POST[domain] . "/" . $_POST[localpart] . "/Maildir";
   $pophomepath = $mailroot . $_POST[domain] . "/" . $_POST[localpart];
