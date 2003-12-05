51f012ce5e44332230b867bbbd22e674107cd56c
avleen <avleen>
2003-12-05 15:59:45 +0000

    added patch by Wash to require delete confirmation on accounts

--- vexim/adminuserdelete.php
+++ vexim/adminuserdelete.php
@@ -2,6 +2,8 @@
   include_once dirname(__FILE__) . "/config/variables.php";
   include_once dirname(__FILE__) . "/config/authpostmaster.php";
 
+if ($_GET[confirm] == "1") {
+
   $admincheck_query = "SELECT COUNT(localpart) AS count FROM users
   			WHERE admin='1'
 			AND localpart='$_GET[localpart]'
@@ -30,5 +32,38 @@
     if (!DB::isError($result)) { header ("Location: adminuser.php?deleted=$_GET[localpart]"); }
     else { header ("Location: adminuser.php?faildeleted=$_GET[localpart]"); }
   }
+  
+} else if ($_GET[confirm] == "cancel") {                   
+    header ("Location: adminuser.php?faildeleted=$_GET[localpart]");
+    die;                                                      
+}                                                           
+
 ?>
+<html>
+  <head>
+    <title>Domain Plus!: Confirm Delete</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+  </head>
+    <body>
+    <? include dirname(__FILE__) . "/config/header.php"; ?>
+    <div id='menu'>
+      <a href='adminuseradd.php'>Add User</a><br>
+      <a href='admin.php' title='Change site password'>Main Menu</a><br>
+      <br><a href='logout.php'>Logout</a><br>
+    </div>
+    <div id='Content'>
+      <form name='userdelete' method='get' action='adminuserdelete.php'>
+        <table align="center">
+	  <tr><td colspan='2'>Please confirm deleting user <?=$_GET[localpart]?>@<?=$_COOKIE[vexim][1]?>:</td></tr>
+	  <tr><td><input name='confirm' type='radio' value='cancel' checked><b> Do Not Delete <? print $_GET[localpart]; ?>@<?=$_COOKIE[vexim][1]?></b></td></tr>
+	  <tr><td><input name='confirm' type='radio' value='1'><b> Delete <? print $_GET[localpart]; ?>@<?=$_COOKIE[vexim][1]?></b></td></tr>
+	  <tr><td><input name='domain' type='hidden' value='<?=$_COOKIE[vexim][1]?>'>
+	      <input name='domain_id' type='hidden' value='<?=$_COOKIE[vexim][2]?>'>
+	      <input name='localpart' type='hidden' value='<?=$_GET[localpart]?>'>
+	      <input name='submit' type='submit' value='Continue'></td></tr>
+	</table>
+      </form>
+    </div>
+  </body>
+</html>
 <!-- Layout and CSS tricks obtained from http://www.bluerobot.com/web/layouts/ -->
