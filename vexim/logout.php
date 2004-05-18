7743623335f7ac3fa93d356a333cadfb3cfcdbe7
avleen <avleen>
2004-05-18 22:30:36 +0000

    added session destroying

--- vexim/logout.php
+++ vexim/logout.php
@@ -1,8 +1,11 @@
 <?
+    include_once dirname(__FILE__) . "/config/httpheaders.php";
     setcookie ("vexim[0]", $_POST['localpart'], time()-86400);
     setcookie ("vexim[1]", $cookierow['domain'], time()-86400);
     setcookie ("vexim[2]", $cookierow['domain_id'], time()-86400);
     setcookie ("vexim[3]", $cryptedpass, time()-86400);
+    $_SESSION = array();
+    session_destroy();
     header ("Location: index.php");
 ?>
 <!-- Layout and CSS tricks obtained from http://www.bluerobot.com/web/layouts/ -->
