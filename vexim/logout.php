11a681a340f220a00269bcedfe6250ba8f9a27a9
avleen <avleen>
2004-07-14 11:32:43 +0000

    switched order so if session destroy fails, you're still redirected.

--- vexim/logout.php
+++ vexim/logout.php
@@ -1,7 +1,7 @@
 <?
     include_once dirname(__FILE__) . "/config/httpheaders.php";
     $_SESSION = array();
-    session_destroy();
     header ("Location: index.php");
+    session_destroy();
 ?>
 <!-- Layout and CSS tricks obtained from http://www.bluerobot.com/web/layouts/ -->
