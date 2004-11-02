822913ced9baae2bc0c1413cadec461a1697364f
avleen <avleen>
2004-11-02 12:07:47 +0000

    multiple admins patch

--- vexim/site.php
+++ vexim/site.php
@@ -47,8 +47,8 @@
 	<th><?php echo _("Total admins"); ?></th>
       </tr>
       <?php
-        $query = "SELECT localpart, domain, domains.domain_id, 
-                         count(*) AS count    
+        $query = "SELECT MIN(localpart) AS localpart, domain,
+	                 domains.domain_id, count(*) AS count    
                   FROM   users, domains
                   WHERE  users.domain_id = domains.domain_id
                   AND    domain !='admin' AND admin=1";
@@ -56,7 +56,7 @@
             $query .= " AND lower(domain) LIKE lower('$letter%')";
         elseif ($_POST['searchfor'] != '')
             $query .= " AND domain LIKE '%" . $_POST['searchfor'] . "%' ";
-        $query .= " GROUP BY domains.domain ORDER BY domain";
+        $query .= " GROUP BY domains.domain, domains.domain_id ORDER BY domain";
   	$result = $db->query($query);
 	if ($result->numRows()) {
 	  while ($row = $result->fetchRow()) {
@@ -64,7 +64,11 @@
 	    print "\t<td><a href=\"sitedelete.php?domain_id={$row['domain_id']}&domain={$row['domain']}\">";
 	    print "<img style='border:0;width:10px;height:16px' title='Delete {$row['domain']}' src='images/trashcan.gif' alt='trashcan'></a></td>\n";
 	    print "\t<td><a href=\"sitechange.php?domain_id={$row['domain_id']}&domain={$row['domain']}\">{$row['domain']}</a></td>\n";
-	    print "\t<td>{$row['localpart']}@{$row['domain']}</td>\n";
+	    if ($row['count'] == 1) {
+              print "\t<td>{$row['localpart']}@{$row['domain']}</td>\n";
+	    } else {
+	      print "\t<td>" . _("Multiple admins") . "</td>\n";
+	    }
 	    print "\t<td>{$row['count']}</td>\n";
 	    print "</tr>\n";
 	  }
@@ -84,7 +88,7 @@
 	  while ($row = $result->fetchRow()) {
 	    print "<tr>";
 	    print "\t<td><a href=\"sitedelete.php?domain_id={$row['domain_id']}&domain={$row['domain']}&type=relay\">";
-	    print "<img style=\"border:0;width:10px;height:16px\" title=\"" . _("Delete %s", $row['domain']) . "\" src=\"images/trashcan.gif\" alt=\"trashcan\"></a></td>\n";
+	    print "<img style=\"border:0;width:10px;height:16px\" title=\"" . _("Delete") . $row['domain'] . "\" src=\"images/trashcan.gif\" alt=\"trashcan\"></a></td>\n";
 	    print "\t<td>{$row['domain']}</td>\n";
 	    print "</tr>\n";
 	  }
@@ -101,7 +105,7 @@
 	  while ($row = $result->fetchRow()) {
 	    print "<tr>";
 	    print "\t<td><a href=\"sitedelete.php?domain_id={$row['domain_id']}&domain={$row['alias']}&type=alias\">";
-	    print "<img style=\"border:0;width:10px;height:16px\" title=\"" . _("Delete %s", $row['alias']) . "\" src=\"images/trashcan.gif\" alt=\"trashcan\"></a></td>\n";
+	    print "<img style=\"border:0;width:10px;height:16px\" title=\"" . _("Delete") . $row['alias'] . "\" src=\"images/trashcan.gif\" alt=\"trashcan\"></a></td>\n";
 	    print "\t<td colspan=\"3\">{$row['alias']} -> {$row['domain']}</td>\n";
 	    print "</tr>\n";
 	  }
