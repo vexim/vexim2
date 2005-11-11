fe126728ed17745abff61516b3398c3f028460bd
avleen <avleen>
2005-11-11 05:25:39 +0000

    Code style conformity

--- vexim/siteaddsubmit.php
+++ vexim/siteaddsubmit.php
@@ -4,13 +4,35 @@
   include_once dirname(__FILE__) . "/config/functions.php";
   include_once dirname(__FILE__) . "/config/httpheaders.php";
 
-  if (isset($_POST['avscan'])) {$_POST['avscan'] = 1;} else {$_POST['avscan'] = 0;}
-  if (isset($_POST['spamassassin'])) {$_POST['spamassassin'] = 1;} else {$_POST['spamassassin'] = 0;}
-  if (isset($_POST['enabled'])) {$_POST['enabled'] = 1;} else {$_POST['enabled'] = 0;}
-  if (isset($_POST['pipe'])) {$_POST['pipe'] = 1;} else {$_POST['pipe'] = 0;}
-  if ($_POST['type'] == "relay") {$_POST['clear'] = $_POST['vclear'] = "BLANK";}
-  if ($_POST['type'] == "alias") {$_POST['clear'] = $_POST['vclear'] = "BLANK";}
-  if ($_POST['max_accounts'] == '') {$_POST['max_accounts'] = '0';}
+  if (isset($_POST['avscan'])) {
+    $_POST['avscan'] = 1;
+  } else {
+    $_POST['avscan'] = 0;
+  }
+  if (isset($_POST['spamassassin'])) {
+    $_POST['spamassassin'] = 1;
+  } else {
+    $_POST['spamassassin'] = 0;
+  }
+  if (isset($_POST['enabled'])) {
+    $_POST['enabled'] = 1;
+  } else {
+    $_POST['enabled'] = 0;
+  }
+  if (isset($_POST['pipe'])) {
+    $_POST['pipe'] = 1;
+  } else {
+    $_POST['pipe'] = 0;
+  }
+  if ($_POST['type'] == "relay") {
+    $_POST['clear'] = $_POST['vclear'] = "BLANK";
+  }
+  if ($_POST['type'] == "alias") {
+    $_POST['clear'] = $_POST['vclear'] = "BLANK";
+  }
+  if ($_POST['max_accounts'] == '') {
+    $_POST['max_accounts'] = '0';
+  }
   
 // User can specify either UID, or username, the former being preferred.
 // Using posix_getpwuid/posix_getgrgid even when we have an UID is so we
@@ -39,78 +61,101 @@
     header ("Location: site.php?failuidguid={$_POST['domain']}");
     die;
   }
-		    
-  $smtphomepath = realpath ($_POST['maildir'] . "/") . "/" . $_POST['domain'] . "/" . $_POST['localpart'] . "/Maildir";
-  $pophomepath = realpath ($_POST['maildir'] . "/") . "/" . $_POST['domain'] . "/" . $_POST['localpart'];
+        
+  $smtphomepath = realpath($_POST['maildir'] . "/") .
+    "/" . $_POST['domain'] . "/" . $_POST['localpart'] . "/Maildir";
+  $pophomepath = realpath($_POST['maildir'] . "/") .
+    "/" . $_POST['domain'] . "/" . $_POST['localpart'];
 //Gah. Transactions!! -- GCBirzan
-  if ((validate_password($_POST['clear'], $_POST['vclear'])) && ($_POST['type'] != "alias")) {
+  if ((validate_password($_POST['clear'], $_POST['vclear'])) &&
+    ($_POST['type'] != "alias")) {
     $query = "INSERT INTO domains 
-               (domain, spamassassin, sa_tag, sa_refuse, avscan,
-	       max_accounts, quotas, maildir, pipe, enabled, uid, gid,
-	       type, maxmsgsize)
-    VALUES ('" . $_POST['domain'] . "',
-    {$_POST['spamassassin']},
-    " . ((isset($_POST['sa_tag'])) ? $_POST['sa_tag']  : 0) . ",
-    " . ((isset($_POST['sa_refuse'])) ? $_POST['sa_refuse']  : 0) . ",
-    {$_POST['avscan']},
-    {$_POST['max_accounts']},
-    " . ((isset($_POST['quotas'])) ? $_POST['quotas']  : 0) . ",
-    '" . realpath ($_POST['maildir'] . "/") . "/" . $_POST['domain'] ."',
-    {$_POST['pipe']},
-    {$_POST['enabled']}, $uid, $gid, '{$_POST['type']}',
-    " . ((isset($_POST['maxmsgsize'] )) ? $_POST['maxmsgsize']  : 0) . ")";
+              (domain, spamassassin, sa_tag, sa_refuse, avscan,
+              max_accounts, quotas, maildir, pipe, enabled, uid, gid,
+              type, maxmsgsize)
+              VALUES ('" . $_POST['domain'] . "'," .
+            "{$_POST['spamassassin']},".
+            ((isset($_POST['sa_tag'])) ? $_POST['sa_tag']  : 0) . "," .
+            ((isset($_POST['sa_refuse'])) ? $_POST['sa_refuse']  : 0) . "," .
+            "{$_POST['avscan']}," .
+            "{$_POST['max_accounts']},".
+            ((isset($_POST['quotas'])) ? $_POST['quotas'] : 0) . ",'" .
+            realpath ($_POST['maildir'] . "/") . "/" . $_POST['domain'] ."'," .
+            "{$_POST['pipe']}," .
+            "{$_POST['enabled']}," .
+            "$uid," .
+            "$gid," .
+            "'{$_POST['type']}',".
+            ((isset($_POST['maxmsgsize'])) ? $_POST['maxmsgsize'] : 0) . ")";
+    print $query; die;
     $domresult = $db->query($query);
     if (!DB::isError($domresult)) {
       if ($_POST['type'] == "local") {
-	$query = "INSERT INTO users (domain_id, localpart, username, clear, crypt, uid, gid, smtp, pop, realname, type, admin)
-		  SELECT domain_id, '" . $_POST['localpart'] . "',
-		  '{$_POST['localpart']}@{$_POST['domain']}',
-		  '{$_POST['clear']}',
-		  '". crypt_password($_POST['clear'],$salt) . "',
-		  $uid, $gid,
-		  '{$smtphomepath}', '{$pophomepath}',
-		  'Domain Admin', 'local', 1 FROM domains
-		  WHERE domains.domain = '{$_POST['domain']}'";
+  $query = "INSERT INTO users
+            (domain_id, localpart, username, clear, crypt, uid, gid,
+            smtp, pop, realname, type, admin)
+            SELECT domain_id, '" . $_POST['localpart'] . "'," .
+            "'{$_POST['localpart']}@{$_POST['domain']}'," .
+            "'{$_POST['clear']}'," .
+            "'". crypt_password($_POST['clear'],$salt) . "'," .
+            $uid,
+            $gid,
+            "'{$smtphomepath}', '{$pophomepath}'," .
+            "'Domain Admin', 'local', 1 FROM domains
+            WHERE domains.domain = '{$_POST['domain']}'";
 // Is using indexes worth setting the domain_id by hand? -- GCBirzan
-	$usrresult = $db->query($query);
-	if (DB::isError($usrresult)) {
-	  header ("Location: site.php?failaddedusrerr={$_POST['domain']}");
-	  die;
-	} else {
-	  header ("Location: site.php?added={$_POST['domain']}&type={$_POST['type']}");
-	  $_SESSION['domain'] = $_POST['domain'];
-	  mail("{$_POST['localpart']}@{$_POST['domain']}", "Welcome Domain Admin!",
-	    "$welcome_newdomain", "From: {$_POST['localpart']}@{$_POST['domain']}\r\n");
-	  die;
-	}
+        $usrresult = $db->query($query);
+        if (DB::isError($usrresult)) {
+          header ("Location: site.php?failaddedusrerr={$_POST['domain']}");
+          die;
+        } else {
+          header ("Location: site.php?added={$_POST['domain']}" .
+                  "&type={$_POST['type']}");
+          $_SESSION['domain'] = $_POST['domain'];
+          mail("{$_POST['localpart']}@{$_POST['domain']}",
+                "Welcome Domain Admin!",
+                "$welcome_newdomain",
+                "From: {$_POST['localpart']}@{$_POST['domain']}\r\n");
+          die;
+        }
       } else {
-      	header ("Location: site.php?added={$_POST['domain']}&type={$_POST['type']}");
-	$_SESSION['domain'] = $_POST['domain'];
-	mail("{$_POST['localpart']}@{$_POST['domain']}", "Welcome Domain Admin!",
-	  "$welcome_newdomain", "From: {$_POST['localpart']}@{$_POST['domain']}\r\n");
-	die;
+        header ("Location: site.php?added={$_POST['domain']}" .
+                "&type={$_POST['type']}");
+        $_SESSION['domain'] = $_POST['domain'];
+        mail("{$_POST['localpart']}@{$_POST['domain']}",
+              "Welcome Domain Admin!",
+              "$welcome_newdomain",
+              "From: {$_POST['localpart']}@{$_POST['domain']}\r\n");
+        die;
       }
     } else {
       header ("Location: site.php?failaddeddomerr={$_POST['domain']}");
       die;
     }
   } else if ($_POST['type'] == "alias") {
-    $idquery = "SELECT domain_id FROM domains WHERE domain = '{$_POST['aliasdest']}' AND domain_id > 1";
+    $idquery = "SELECT domain_id FROM domains
+                WHERE domain = '{$_POST['aliasdest']}'
+                AND domain_id > 1";
     $idresult = $db->query($idquery);
     if (DB::isError($idresult)) {
       header ("Location: site.php?baddestdom={$_POST['domain']}");
       die;
     } else {
       $idrow = $idresult->fetchRow();
-      if (!isset($idrow['domain_id'])) { header ("Location: site.php?baddestdom={$_POST['domain']}"); die; }
+      if (!isset($idrow['domain_id'])) {
+        header ("Location: site.php?baddestdom={$_POST['domain']}");
+        die;
+      }
     }
-    $query = "INSERT INTO domainalias (domain_id, alias) values ('{$idrow['domain_id']}', '{$_POST['domain']}')";
+    $query = "INSERT INTO domainalias (domain_id, alias)
+              VALUES ('{$idrow['domain_id']}', '{$_POST['domain']}')";
     $result = $db->query($query);
     if (DB::isError($result)) {
       header ("Location: site.php?failaddeddomerr={$_POST['domain']}");
       die;
     } else {
-      header ("Location: site.php?added={$_POST['domain']}&type={$_POST['type']}");
+      header ("Location: site.php?added={$_POST['domain']}" .
+              "&type={$_POST['type']}");
       die;
     }
   } else {
