35109da7bfff02fa9a92f1ff100ebf65a7fbc939
avleen <avleen>
2006-06-15 18:33:38 +0000

    fixed missing parenthesis

--- vexim/userchangesubmit.php
+++ vexim/userchangesubmit.php
@@ -12,7 +12,7 @@
   $row = $result->fetchRow();
   if ((isset($_POST['on_avscan'])) && ($row['avscan'] = 1)) {$_POST['on_avscan'] = 1;} else {$_POST['on_avscan'] = 0;}
   if ((isset($_POST['on_spamassassin'])) && ($row['spamassassin'] = 1)) {$_POST['on_spamassassin'] = 1;} else {$_POST['on_spamassassin'] = 0;}
-	if ((isset($_POST['maxmsgsize'])) && ($_POST['maxmsgsize'] > $row['maxmsgsize']) {$_POST['maxmsgsize'] = $row['maxmsgsize'];}
+  if ((isset($_POST['maxmsgsize'])) && ($_POST['maxmsgsize'] > $row['maxmsgsize'])) {$_POST['maxmsgsize'] = $row['maxmsgsize'];}
 
   if ($_POST['realname'] != "") {
     $query = "UPDATE users SET realname='{$_POST['realname']}'
