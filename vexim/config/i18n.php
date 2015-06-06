8ed4ddd5e026a1d6d338b30d3a4b9c995b1ae443
kvsmirnov <skv@ivsor.net>
2015-06-07 00:42:57 +0300

    Resolved problem with Cyrillic letters
    Some times Cyrillic letters sends in KOI8-R from web server but in
    headers was UTF-8

--- vexim/config/i18n.php
+++ vexim/config/i18n.php
@@ -3,8 +3,9 @@
 $language = 'ru_RU';
 #$language = 'en_EN';
 putenv ("LANG=$language");
-setlocale(LC_ALL, "");
+setlocale(LC_ALL, "$language.UTF-8");
 bindtextdomain('messages', './locale');
+bind_textdomain_codeset('messages', 'UTF-8');
 textdomain('messages');
 
 ?>
