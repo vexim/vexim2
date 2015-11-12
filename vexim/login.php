d95dee27c172d787f64f4356e481052e81367b48
udera <udera@aspekte.net>
2015-11-12 20:24:14 +0000

    Don't allow user login for disabled domain\nFix indenting

--- vexim/login.php
+++ vexim/login.php
@@ -14,22 +14,23 @@
 
 	# construct the correct sql statement based on who the user is
   if ($_POST['localpart'] === 'siteadmin') {
-		$query = "SELECT crypt,localpart,user_id,domain,domains.domain_id,users.admin,users.type,domains.enabled AS domainenabled FROM users,domains
-      WHERE localpart='siteadmin'
-      AND domain='admin'
-      AND username='siteadmin'
-      AND users.domain_id = domains.domain_id";
+    $query = "SELECT crypt,localpart,user_id,domain,domains.domain_id,users.admin,users.type,domains.enabled AS domainenabled FROM users,domains
+    WHERE localpart='siteadmin'
+    AND domain='admin'
+    AND username='siteadmin'
+    AND users.domain_id = domains.domain_id";
   } else if ($AllowUserLogin) {
-		$query = "SELECT crypt,localpart,user_id,domain,domains.domain_id,users.admin,users.type,domains.enabled AS domainenabled FROM users,domains
-      WHERE localpart=:localpart
-      AND users.domain_id = domains.domain_id
-      AND domains.domain=:domain;";
+    $query = "SELECT crypt,localpart,user_id,domain,domains.domain_id,users.admin,users.type,domains.enabled AS domainenabled, users.enabled AS userenabled
+    FROM users,domains
+    WHERE localpart=:localpart
+    AND users.domain_id = domains.domain_id
+    AND domains.domain=:domain;";
   } else {
-		$query = "SELECT crypt,localpart,user_id,domain,domains.domain_id,users.admin,users.type,domains.enabled AS domainenabled FROM users,domains
-      WHERE localpart=:localpart
-      AND users.domain_id = domains.domain_id
-      AND domains.domain=:domain
-      AND admin=1;";
+    $query = "SELECT crypt,localpart,user_id,domain,domains.domain_id,users.admin,users.type,domains.enabled AS domainenabled FROM users,domains
+    WHERE localpart=:localpart
+    AND users.domain_id = domains.domain_id
+    AND domains.domain=:domain
+    AND admin=1;";
   }
   $sth = $dbh->prepare($query);
   $success = $sth->execute(array(':localpart'=>$_POST['localpart'], ':domain'=>$_POST['domain']));
@@ -44,7 +45,7 @@
   }
 
   $row = $sth->fetch();
-	
+
   $cryptedpass = crypt_password($_POST['crypt'], $row['crypt']);
 
 //  Some debugging prints. They help when you don't know why auth is failing.
@@ -59,39 +60,43 @@
   print $cryptedpass . "<br>\n";
   */
 
-	# if they have the wrong password bail out
-	if ($cryptedpass !== $row['crypt']) {
-		header ('Location: index.php?login=failed');
-		die();
-	}
+  # if they have the wrong password bail out
+  if ($cryptedpass !== $row['crypt']) {
+    header ('Location: index.php?login=failed');
+    die();
+  }
 
-	# populate session variables from what was retrieved from the database (NOT what they posted)
-    $_SESSION['localpart'] = $row['localpart'];
-    $_SESSION['domain'] = $row['domain'];
-    $_SESSION['domain_id'] = $row['domain_id'];
-	$_SESSION['crypt'] = $row['crypt'];
-    $_SESSION['user_id'] = $row['user_id'];
+  # populate session variables from what was retrieved from the database (NOT what they posted)
+  $_SESSION['localpart'] = $row['localpart'];
+  $_SESSION['domain'] = $row['domain'];
+  $_SESSION['domain_id'] = $row['domain_id'];
+  $_SESSION['crypt'] = $row['crypt'];
+  $_SESSION['user_id'] = $row['user_id'];
 
-	# redirect the user to the correct starting page
-	if (($row['admin'] == '1') && ($row['type'] == 'site')) {
-                if($_POST['crypt']=="CHANGE") {
-                  header ('Location: sitepassword.php');
-                  die();
-                }
-		header ('Location: site.php');
-		die();
-	} 
-	if ($row['admin'] == '1') {
-		header ('Location: admin.php');
-		die();
+  # redirect the user to the correct starting page
+  if (($row['admin'] === '1') && ($row['type'] === 'site')) {
+    if($_POST['crypt'] === "CHANGE") {
+      header ('Location: sitepassword.php');
+      die();
     }
-	if (($row['domainenabled'] == '0')) {
-		header ('Location: index.php?domaindisabled');
-		die();
-}
-	
-	# must be a user, send them to edit their own details
-	header ('Location: userchange.php');
-	
+    header ('Location: site.php');
+    die();
+  }
+  if ($row['admin'] === '1') {
+    header ('Location: admin.php');
+    die();
+ }
+ if (($row['userenabled'] === '0')) {
+   header ('Location: index.php');
+   die();
+ }
+ if (($row['domainenabled'] === '0')) {
+   header ('Location: index.php?domaindisabled');
+   die();
+ }
+
+ # must be a user, send them to edit their own details
+ header ('Location: userchange.php');
+
 ?>
 <!-- Layout and CSS tricks obtained from http://www.bluerobot.com/web/layouts/ -->
