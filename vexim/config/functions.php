7f5648aca92d8a145c8e60a2434cfe0f011904ff
runout-at <markus@runout.at>
2016-06-15 03:17:47 +0200

    comment added

--- vexim/config/functions.php
+++ vexim/config/functions.php
@@ -35,6 +35,18 @@
         ) { return TRUE; }
 
         return FALSE;
+/*
+    Explaining $\S*(?=\S{8,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])(?=\S*[\W])\S*$
+    $ = beginning of string
+    \S* = any set of characters
+    (?=\S{8,}) = of at least length 8
+    (?=\S*[a-z]) = containing at least one lowercase letter
+    (?=\S*[A-Z]) = and at least one uppercase letter
+    (?=\S*[\d]) = and at least one number
+    (?=\S*[\W]) = and at least a special character (non-word characters)
+    $ = end of the string
+
+ */
     }
     
     /**
