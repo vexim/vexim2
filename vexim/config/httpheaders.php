e7b39afb9308a21cd4fc2cb85a1f8607d6317404
udera <udera@aspekte.net>
2017-04-15 22:51:03 +0000

    Add Content Security Policy (CSP)

--- vexim/config/httpheaders.php
+++ vexim/config/httpheaders.php
@@ -2,4 +2,5 @@
   session_start();
   header("Cache-control: private"); // IE6 hack to back forms + BACK button work
   header("Content-Type: text/html; charset=utf-8");
+  if (isset($CSPenabled) && $CSPenabled === true)  header("Content-Security-Policy: default-src 'self';");
 ?>
